18b90c1f141350d05edecda3103e1807e87b617e
DANGHAI11 <haidv@regit-technology.com>
2023-06-05 15:15:55 +0700

    Update fix convention and responsive home page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-function search_mobile(){
-    let search = document.getElementsByClassName("header_right");
-    search[0].classList.toggle("active");
+function searchMobile() {
+  let search = document.getElementsByClassName("header-right");
+  search[0].classList.toggle("active");
 }
 
-function menu_mobile(){
-    let menu = document.getElementsByClassName("header_left");
-    menu[0].classList.add("active");
-    menu[0].style.top = '0';
+function menuMobile() {
+  let menu = document.getElementsByClassName("header-left");
+  menu[0].classList.add("active");
+  menu[0].style.top = "0";
 }
 
-function remove_menu_mobile(){
-    let menu = document.getElementsByClassName("header_left");
-    menu[0].classList.remove("active");
-    menu[0].style.top = '-300px';
+function removeMenuMobile() {
+  let menu = document.getElementsByClassName("header-left");
+  menu[0].classList.remove("active");
+  menu[0].style.top = "-300px";
 }
