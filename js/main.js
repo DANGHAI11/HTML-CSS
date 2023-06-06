9f038d81574991a2dd6c078796f1d99cfa92a170
DANGHAI11 <haidv@regit-technology.com>
2023-06-06 11:59:31 +0700

    update trasition menu

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ function searchMobile() {
 
 function menuMobile() {
   let menu = document.getElementsByClassName("header-left");
+  let close  = document.getElementsByClassName("menu-close");
   menu[0].classList.add("active");
-  menu[0].style.top = "0";
+  menu[0].style.left = "0";
+  close[0].style.display = "block";
 }
 
 function removeMenuMobile() {
   let menu = document.getElementsByClassName("header-left");
+  let close  = document.getElementsByClassName("menu-close");
   menu[0].classList.remove("active");
-  menu[0].style.top = "-300px";
+  menu[0].style.left = "-300px";
+  close[0].style.display = "none";
 }
