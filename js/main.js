4528465e95e2b648288cea6cafc63772af5537f4
DANGHAI11 <haidv@regit-technology.com>
2023-06-07 14:50:35 +0700

    Update fix responsive mobile and fix page home desktop

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
+let menu = document.getElementsByClassName("header-left");
+let background = document.getElementsByClassName("background-mobile");
+var prevScrollpos = window.pageYOffset;
+
 function searchMobile() {
   let search = document.getElementsByClassName("header-right");
   search[0].classList.toggle("active");
 }
 
 function menuMobile() {
-  let menu = document.getElementsByClassName("header-left");
-  let close = document.getElementsByClassName("menu-close");
-  menu[0].classList.add("active");
-  menu[0].style.left = "0";
-  close[0].style.display = "block";
+  menu[0].classList.toggle("active");
+  background[0].style.display = "block";
 }
 
 function removeMenuMobile() {
-  let menu = document.getElementsByClassName("header-left");
-  let close = document.getElementsByClassName("menu-close");
   menu[0].classList.remove("active");
-  menu[0].style.left = "-300px";
-  close[0].style.display = "none";
+  background[0].style.display = "none";
+}
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header .nav").style.top = "0";
+  } else {
+    document.querySelector("header .nav").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
